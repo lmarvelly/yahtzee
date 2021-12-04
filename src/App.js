c1e98ae70fe5d10416ffc612a0a31aa997d05fcc
Luke Marvelly <lmarvelly@hotmail.com>
2021-12-04 09:57:50 +0000

    Refactoring

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import Game from "./components/Game";
-import "./App.css";
+import "./styles/App.css";
 
 config.autoAddCss = false;
 
